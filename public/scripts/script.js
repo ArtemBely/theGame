7d8c943c4ef2c139551bb9dc9ea9553058554977
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-12-12 11:52:31 +0500

    Map & pre-production

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,9 @@
-// if(document.body.classList.contains('preview')) {
-//   
-//   wrap.scrollTo(0, 0)
-//   console.log(wrap.offsetHeight - wrap.offsetTop)
-// }
-
 const wrap = document.querySelector('.wrap_allScreens')
 
 wrap.addEventListener('click', function() {
-  console.log(wrap.scrollTop)
+  console.log(wrap.getBoundingClientRect().top) 
 })
+
+window.onload = function() {
+  wrap.scrollTo(0, 0);
+}
