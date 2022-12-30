7574c5b693b0b9d3b51cae2359b330f3037d45db
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-12-30 13:26:02 +0500

    finished animation & map title

--- public/scripts/sphere.js
+++ public/scripts/sphere.js
@@ -3,12 +3,11 @@ import {
         PerspectiveCamera,
         TextureLoader,
         SphereGeometry,
-        Matrix4,
         ShaderMaterial,
         Mesh,
         WebGLRenderer,
-        BufferAttribute
-        } from 'https://unpkg.com/three/build/three.module.js'
+        BufferAttribute,
+        } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.148.0/three.module.min.js'
 
 import getVertexShader from './vertexShader.js'
 import getFragmentShader from './fragmentShader.js'
@@ -17,7 +16,7 @@ const canvas = document.querySelector('.webgl');
 
 const scene = new Scene();
 
-const displayWidth = (window.innerWidth > 1950) ? 3200 : (window.innerWidth > 768) ? 1600 : 1000                
+const displayWidth = (window.innerWidth > 1920) ? 3200 : (window.innerWidth > 768) ? 1920 : 1000                
 
 let { width, height } = {
 width: displayWidth,
@@ -29,51 +28,47 @@ scene.add(camera);
 
 
 const textureLoader = new TextureLoader();
-const texture = textureLoader.load("public/images/sphere_texture.jpg");
+const texture = textureLoader.load("public/images/Group 60.jpg");
 textureLoader.setCrossOrigin('anonymous');
 
-const geometry = new SphereGeometry(1, 250, 250);
+const geometry = new SphereGeometry(1, 280, 280);
 
 let time = 0.2;
 
 const uniforms = {
 uTime: { value: time },
 uDistortionFrequency: { value: 1 },
-uDistortionStrngth: { value: 1.5 },
 uDisplacementFrequency: { value: 3 },
 uDisplacementStrngth: { value: 0.18 },
 uSubdivision: {
-value: {x: 250, y: 250},
+value: {x: 280, y: 280},
 },
-uPos: {value: {}},
-uFresnelOffset: { value: -2 },
+uFresnelOffset: { value: -1.9 },
 uFresnelMultiplier: { value: 3.587 },
 uFresnelPower: { value: 1.3 },
 uLightColor: { value: {x: 2.37, y: 2.25, z: 1.58, w:1.0} },
 uLightAPosition: { value: {x: 1.0, y: 1.0, z: 0.0} },
-uLightBPosition: { value: {x: -1.0, y: -5.0, z: 0.0} },
+uLightBPosition: { value: {x: -1.0, y: -5.5, z: 0.0} },
 uTexture: { type: 't', value: texture },
 viewMatrixCamera: { type: 'm4', value: camera.matrixWorldInverse },
 projectionMatrixCamera: { type: 'm4', value: camera.projectionMatrix },
-modelMatrixCamera: { type: 'mat4', value: camera.matrixWorld },
-savedModelMatrix: { type: 'mat4', value: new Matrix4() },
-projPosition: { type: 'v3', value: camera.position },
+savedModelMatrix: { type: 'mat4', value:  [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1] },
 };
-
-console.log(geometry.attributes)
 
 if(window.Worker) {
   const worker = new Worker('./public/scripts/worker.js', {type: 'module'})
   worker.postMessage({ type: 'tangents' });
-
   worker.onmessage = (message) => {
     if(message.data.type === 'tangents') {
       geometry.setAttribute('tangent', new BufferAttribute(message.data.object, 4));
+    } else if (message.data.type === 'texture') {
+      
     }
   };  
 } else {
   geometry.computeTangents();
 }
+
 
 const material = new ShaderMaterial({
 uniforms: uniforms,
@@ -103,39 +98,69 @@ camera.updateWorldMatrix();
 renderer.setSize(width, height);
 });
 
-function sphereZoom() {
+function sphereZoom(anchor) {
   canvas.style.zIndex = 5;
   uniforms.uTime.value = 0.5; 
-  gsap.to(camera.position,
-  {
-    duration : 0.4,
-    delay: 0.1,
-    z : 1.15,
-    ease: "power3.inOut",
-    onComplete: () => {
-      document.body.classList.remove("preview");
-      document.querySelector('.firstScreen').classList.add('opened')
-    }
+  if (gsap) {
+    gsap.to(camera.position,
+      {
+        duration : 0.4,
+        delay: 0.1,
+        z : 1.15,
+        ease: "power3.inOut",
+        onComplete: () => {
+          document.body.classList.remove("preview");
+          document.querySelector('.firstScreen').classList.add('opened')
+          if(anchor) {
+            document.querySelector(anchor).scrollIntoView(
+              {
+               behavior: 'smooth',
+               block: 'center'
+              }
+            )
+          }
+        }
+      }
+    )
   }
-  )
 }
 
+function stopableEventListener(item, e, callback) {
+  item.addEventListener(e, callback); 
+  return function () {
+    item.removeEventListener(e, callback);
+  }
+}
 
-document.addEventListener('wheel', (event) => {
-event.stopPropagation();
-sphereZoom();
-});
+const wheel = stopableEventListener(document, 'wheel', () => 
+  {
+   sphereZoom(); 
+   wheel(); 
+  }
+);
+const touch = stopableEventListener(document, 'touchend', (e) => 
+  {
+    let anchor;
+    if(e.target.id === 'scroll-to-about') {
+       anchor = '#about';
+    } else if(e.target.id ==='long-link' || 'short-link') return;
+    sphereZoom(anchor); 
+    touch(); 
+  }
+);
+const click = stopableEventListener(document.querySelector('#scroll-to-about'), 'click', () => 
+  { 
+    sphereZoom('#about'); 
+    click(); 
+  }
+);
 
-document.addEventListener('touchstart', (event) => {
-event.stopPropagation();
-sphereZoom();
-});
 
 let requestID;
 
 const loop = () => {
 requestID = window.requestAnimationFrame(loop);
-uniforms.uTime.value += 0.015;
+uniforms.uTime.value += 0.007;
 
 if(!document.body.classList.contains("preview")) {
   disableLoop(requestID);
@@ -145,7 +170,7 @@ renderer.render(scene, camera);
 };
 
 function disableLoop(id) {
-  window.cancelAnimationFrame(id)
+  window.cancelAnimationFrame(id);
   requestID = null;
 }
 
--- public/scripts/vertexShader.js
+++ public/scripts/vertexShader.js
@@ -3,45 +3,38 @@ export default function getVertexShader() {
   #define M_PI 3.1415926535897932384626433832795
   
   uniform mat4 savedModelMatrix;
-  
   uniform float uTime;
-  
   uniform float uDisplacementStrngth;
   uniform float uDisplacementFrequency;
   uniform float uDistortionFrequency;
-  uniform float uDistortionStrngth;
-  
   uniform vec4 uLightColor;
   uniform vec3 uLightAPosition;
   uniform vec3 uLightBPosition;
   uniform vec2 uSubdivision;
-  
   uniform vec3 uOffset;
-  
   uniform float uFresnelOffset;
   uniform float uFresnelMultiplier;
   uniform float uFresnelPower;
+  uniform float uPos;
   
   attribute vec4 tangent;
   
   varying vec3 vNormal;
   varying float vPerlinStrength;
-  varying vec2 vUv;
   varying float vFresnel;
   varying float vLightAIntensity;
   varying float vLightBIntensity;
+  varying vec4 vWorldPosition;
+  varying vec4 vTexCoords;
+  varying vec2 vUv;
   
   uniform mat4 viewMatrixCamera;
   uniform mat4 projectionMatrixCamera;
   uniform mat4 modelMatrixCamera;
-  
-  varying vec4 vWorldPosition;
-  varying vec4 vTexCoords;
-  
+
   vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
   vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
   vec4 fade(vec4 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
-
   float perlin4d(vec4 P){
   vec4 Pi0 = floor(P); 
   vec4 Pi1 = Pi0 + 1.0;
@@ -54,8 +47,7 @@ export default function getVertexShader() {
   vec4 iz0 = vec4(Pi0.zzzz);
   vec4 iz1 = vec4(Pi1.zzzz);
   vec4 iw0 = vec4(Pi0.wwww);
-  vec4 iw1 = vec4(Pi1.wwww);
-  
+  vec4 iw1 = vec4(Pi1.wwww);  
   vec4 ixy = permute(permute(ix) + iy);
   vec4 ixy0 = permute(ixy + iz0);
   vec4 ixy1 = permute(ixy + iz1);
@@ -63,7 +55,6 @@ export default function getVertexShader() {
   vec4 ixy01 = permute(ixy0 + iw1);
   vec4 ixy10 = permute(ixy1 + iw0);
   vec4 ixy11 = permute(ixy1 + iw1);
-  
   vec4 gx00 = ixy00 / 7.0;
   vec4 gy00 = floor(gx00) / 7.0;
   vec4 gz00 = floor(gy00) / 6.0;
@@ -74,7 +65,6 @@ export default function getVertexShader() {
   vec4 sw00 = step(gw00, vec4(0.0));
   gx00 -= sw00 * (step(0.0, gx00) - 0.5);
   gy00 -= sw00 * (step(0.0, gy00) - 0.5);
-  
   vec4 gx01 = ixy01 / 7.0;
   vec4 gy01 = floor(gx01) / 7.0;
   vec4 gz01 = floor(gy01) / 6.0;
@@ -85,7 +75,6 @@ export default function getVertexShader() {
   vec4 sw01 = step(gw01, vec4(0.0));
   gx01 -= sw01 * (step(0.0, gx01) - 0.5);
   gy01 -= sw01 * (step(0.0, gy01) - 0.5);
-  
   vec4 gx10 = ixy10 / 7.0;
   vec4 gy10 = floor(gx10) / 7.0;
   vec4 gz10 = floor(gy10) / 6.0;
@@ -96,7 +85,6 @@ export default function getVertexShader() {
   vec4 sw10 = step(gw10, vec4(0.0));
   gx10 -= sw10 * (step(0.0, gx10) - 0.5);
   gy10 -= sw10 * (step(0.0, gy10) - 0.5);
-  
   vec4 gx11 = ixy11 / 7.0;
   vec4 gy11 = floor(gx11) / 7.0;
   vec4 gz11 = floor(gy11) / 6.0;
@@ -107,7 +95,6 @@ export default function getVertexShader() {
   vec4 sw11 = step(gw11, vec4(0.0));
   gx11 -= sw11 * (step(0.0, gx11) - 0.5);
   gy11 -= sw11 * (step(0.0, gy11) - 0.5);
-  
   vec4 g0000 = vec4(gx00.x,gy00.x,gz00.x,gw00.x);
   vec4 g1000 = vec4(gx00.y,gy00.y,gz00.y,gw00.y);
   vec4 g0100 = vec4(gx00.z,gy00.z,gz00.z,gw00.z);
@@ -124,31 +111,15 @@ export default function getVertexShader() {
   vec4 g1011 = vec4(gx11.y,gy11.y,gz11.y,gw11.y);
   vec4 g0111 = vec4(gx11.z,gy11.z,gz11.z,gw11.z);
   vec4 g1111 = vec4(gx11.w,gy11.w,gz11.w,gw11.w);
-  
   vec4 norm00 = taylorInvSqrt(vec4(dot(g0000, g0000), dot(g0100, g0100), dot(g1000, g1000), dot(g1100, g1100)));
-  g0000 *= norm00.x;
-  g0100 *= norm00.y;
-  g1000 *= norm00.z;
-  g1100 *= norm00.w;
-  
-  vec4 norm01 = taylorInvSqrt(vec4(dot(g0001, g0001), dot(g0101, g0101), dot(g1001, g1001), dot(g1101, g1101)));
-  g0001 *= norm01.x;
-  g0101 *= norm01.y;
-  g1001 *= norm01.z;
-  g1101 *= norm01.w;
-  
-  vec4 norm10 = taylorInvSqrt(vec4(dot(g0010, g0010), dot(g0110, g0110), dot(g1010, g1010), dot(g1110, g1110)));
-  g0010 *= norm10.x;
-  g0110 *= norm10.y;
-  g1010 *= norm10.z;
-  g1110 *= norm10.w;
-  
+  g0000 *= norm00.x;g0100 *= norm00.y;g1000 *= norm00.z;g1100 *= norm00.w;
+  vec4 norm01 = taylorInvSqrt(vec4(dot(g0001, g0001), dot(g0101, g0101), dot(g1001, g1001), dot(g1101, g1101)));g0001 *= norm01.x;g0101 *= norm01.y;g1001 *= norm01.z;g1101 *= norm01.w;
+  vec4 norm10 = taylorInvSqrt(vec4(dot(g0010, g0010), dot(g0110, g0110), dot(g1010, g1010), dot(g1110, g1110)));g0010 *= norm10.x;g0110 *= norm10.y;g1010 *= norm10.z;g1110 *= norm10.w;
   vec4 norm11 = taylorInvSqrt(vec4(dot(g0011, g0011), dot(g0111, g0111), dot(g1011, g1011), dot(g1111, g1111)));
   g0011 *= norm11.x;
   g0111 *= norm11.y;
   g1011 *= norm11.z;
   g1111 *= norm11.w;
-  
   float n0000 = dot(g0000, Pf0);
   float n1000 = dot(g1000, vec4(Pf1.x, Pf0.yzw));
   float n0100 = dot(g0100, vec4(Pf0.x, Pf1.y, Pf0.zw));
@@ -165,7 +136,6 @@ export default function getVertexShader() {
   float n1011 = dot(g1011, vec4(Pf1.x, Pf0.y, Pf1.zw));
   float n0111 = dot(g0111, vec4(Pf0.x, Pf1.yzw));
   float n1111 = dot(g1111, Pf1);
-  
   vec4 fade_xyzw = fade(Pf0);
   vec4 n_0w = mix(vec4(n0000, n1000, n0100, n1100), vec4(n0001, n1001, n0101, n1101), fade_xyzw.w);
   vec4 n_1w = mix(vec4(n0010, n1010, n0110, n1110), vec4(n0011, n1011, n0111, n1111), fade_xyzw.w);
@@ -175,7 +145,6 @@ export default function getVertexShader() {
   return 2.2 * n_xyzw;
   }
   
-  
   vec3 getDisplacedPosition(vec3 _position) {
   vec3 displacementPosition = _position;
   displacementPosition += perlin4d(vec4(displacementPosition * uDistortionFrequency, uTime * 0.4)) * uDisplacementStrngth;
@@ -184,21 +153,20 @@ export default function getVertexShader() {
   displacedPosition += perlinStrength * normalize(_position) * uDisplacementStrngth;
   return displacedPosition;
   }
-  
+  uniform sampler2D uTexture;
+  varying vec4 vColor; 
   void main() {
   vec3 displacedPosition = getDisplacedPosition(position);
   vec4 viewPosition = viewMatrix * vec4(displacedPosition, 1.0);
   vec4 projectedPosition = projectionMatrix * viewPosition;
-
   gl_Position = projectedPosition;
-  
+
   float distanceA = (M_PI * 2.0) / uSubdivision.x;
   float distanceB = M_PI / uSubdivision.x;
   vec3 biTangent = cross(normal, tangent.xyz);
-  
   vec3 positionA = position + tangent.xyz * distanceA;
   vec3 displacedPositionA = getDisplacedPosition(positionA);
-  
+
   vec3 positionB = position + biTangent.xyz * distanceB;
   vec3 displacedPositionB = getDisplacedPosition(positionB);
   
@@ -214,9 +182,13 @@ export default function getVertexShader() {
   
   vWorldPosition = savedModelMatrix * vec4(position, 1.0);
   vTexCoords = projectionMatrixCamera * viewMatrixCamera * vWorldPosition;
+
+  vec2 uv = (vTexCoords.xy / vTexCoords.w) * 0.5 + 0.5;
+
   vFresnel = fresnel;
   vLightAIntensity = lightAIntensity;
   vLightBIntensity = lightBIntensity;
+  vUv = uv;
   }
   `;
   }
